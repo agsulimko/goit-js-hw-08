8b807a7ccaa51e8e15d4badb3d48facee903ad02
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-07-14 14:45:06 +0200

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,30 +5,38 @@ const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 //  input.addEventListener("input", throttle(storageFormData, 500));
 
-form.addEventListener("submit", throttle(onFormSubmit), 500);
+form.addEventListener("submit", onFormSubmit);
+// form.addEventListener('input', throttle(storageFormData, 500));
 // проверяем состояние хранилища
 
 // const storage = localStorage.getItem(LOCAL_KEY);
 // console.log(storage);
-const storageArr = JSON
-    .parse(localStorage.getItem(LOCAL_KEY)) ?? [];
-    console.log(storageArr);
-console.log(storageArr.email);
-console.log(storageArr.message);
-if (storageArr) {
-    input.setAttribute('value', storageArr.email);
-    // console.log(textarea);
-    textarea.innerHTML = `${storageArr.message}`;
-//    Чистим хранилище
-    function removeLocalStorage() {
-        localStorage.removeItem(LOCAL_KEY);
-        console.log('хранилище очищено!', localStorage.getItem(LOCAL_KEY));
-        return;
-    };
-    removeLocalStorage();
-};
-    
 
+    const storageArr = JSON
+        .parse(localStorage.getItem(LOCAL_KEY)) ?? [];
+    console.log(storageArr);
+    console.log(storageArr.email);
+    console.log(storageArr.message);
+if (storageArr) {
+   
+              input.setAttribute('value', storageArr.email);
+        // console.log(textarea);
+    textarea.innerHTML = `${storageArr.message}`;
+  
+    
+      
+     
+
+        //    Чистим хранилище
+    function removeLocalStorage() {
+            if (localStorage.getItem(LOCAL_KEY)){
+            localStorage.removeItem(LOCAL_KEY);
+            console.log('хранилище очищено!', localStorage.getItem(LOCAL_KEY));
+            return;
+        };}
+          removeLocalStorage();
+ }; 
+    
 function onFormSubmit(event) {
     event.preventDefault();
     // console.log(event.currentTarget);
@@ -54,7 +62,7 @@ function onFormSubmit(event) {
     localStorage.setItem(LOCAL_KEY, JSON.stringify(arrayForm));
     
     //    Чистим поля формы
-   
+//    event.currentTarget.reset();
 // 2 -й вариант
     if (localStorage.getItem(LOCAL_KEY)) {
         input.removeAttribute("value");
@@ -65,4 +73,4 @@ function onFormSubmit(event) {
     }
 };
 
-// player.on('timeupdate',throttle(onPlay, 1000 ));
+
