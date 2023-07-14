b17156a50eb40c68f058a2ff8ff1e79aaee66bc7
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-07-14 14:19:40 +0200

    u

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,11 @@
+import throttle from 'lodash.throttle';
 const LOCAL_KEY = 'feedback-form-state'
 const form = document.querySelector(".feedback-form");
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 //  input.addEventListener("input", throttle(storageFormData, 500));
 
-form.addEventListener("submit", onFormSubmit);
+form.addEventListener("submit", throttle(onFormSubmit), 500);
 // проверяем состояние хранилища
 
 // const storage = localStorage.getItem(LOCAL_KEY);
@@ -19,9 +20,12 @@ if (storageArr) {
     // console.log(textarea);
     textarea.innerHTML = `${storageArr.message}`;
 //    Чистим хранилище
-       localStorage.removeItem('feedback-form-state');
- console.log(storageArr);
-
+    function removeLocalStorage() {
+        localStorage.removeItem(LOCAL_KEY);
+        console.log('хранилище очищено!', localStorage.getItem(LOCAL_KEY));
+        return;
+    };
+    removeLocalStorage();
 };
     
 
@@ -47,16 +51,18 @@ function onFormSubmit(event) {
 
     console.log(' localStorage:', arrayForm);
 
-    localStorage.setItem
-        (LOCAL_KEY, JSON.stringify(arrayForm));
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(arrayForm));
     
     //    Чистим поля формы
-    input.removeAttribute("value");
-    console.log('Email delete!', email.value);
-    textarea.innerHTML = '';
-     console.log('Messege delete!', email.value);
    
-
+// 2 -й вариант
+    if (localStorage.getItem(LOCAL_KEY)) {
+        input.removeAttribute("value");
+        console.log('Email delete!', email.value);
+        textarea.innerHTML = '';
+        console.log('Messege delete!', email.value);
+        return;
+    }
 };
 
-
+// player.on('timeupdate',throttle(onPlay, 1000 ));
