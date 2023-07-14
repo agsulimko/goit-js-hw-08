44da7e0d7dc148f3513dd2790f7de2942dada390
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-07-14 11:36:04 +0200

    u

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
 
-// import "simplelightbox/dist/simple-lightbox.min.css";
+ import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
